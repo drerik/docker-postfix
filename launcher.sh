52f0a407612ea3c74a9316f6d344e662e3b00756
Diego Pasten <dap@enonic.com>
2018-02-12 15:01:57 +0100

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Setting myhostname variable for postfix"
-postconf -e myhostname=$HOSTNAME
+postconf -e myhostname=$(hostname)
 
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
